d9e0b13288acab016811385dc386ce1bf809e9da
Hunter <ihunt0002@gmail.com>
2022-09-07 12:38:47 -0400

    changed index title

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fetch = require('node-fetch');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Xenobuilds' });
 });
 
 module.exports = router;
