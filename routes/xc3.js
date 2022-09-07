4d0f28e80869fc69e3b6a375193a8ba2569cfe87
Hunter <ihunt0002@gmail.com>
2022-09-07 12:39:09 -0400

    added code to get data from api and send it to pug file and also updated form post

--- routes/xc3.js
+++ routes/xc3.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fetch = require('node-fetch');
 var router = express.Router();
 
 var fs = require('fs');
@@ -6,22 +7,32 @@ var fs = require('fs');
 const party_data = require('../public/json/party_data.json');
 
 /* GET route */
-router.get('/', function(req, res, next) {
-  res.render('xc3', {title: 'XC3'});
+router.get('/', async function(req, res, next) {
+  let jsonBlocks;
+  try {
+    var response = await fetch('https://hunterm64.github.io/data/xc3HeroList.json')
+    jsonBlocks = await response.json();
+    console.log(jsonBlocks);
+  } catch (e) {
+    console.error(e)
+  }
+  res.render('xc3', {title: 'XC3', heroes:jsonBlocks});
 });
 
 router.post('/', function(req, res, next) {
   var party_id = party_data[party_data.length-1].id + 1
-  var party_name = req.body.party_name
+  var partyName = req.body.partyName
+  var heroName = req.body.heroName
 
   const party = {
     id: party_id,
-    name: party_name
+    name: partyName,
+    heroName: heroName
   }
   party_data.push(party)
   const jsonParty = JSON.stringify(party_data)
 
-  fs.writeFile(__dirname + '/../public/json/party_data.json', jsonParty, err => {
+  fs.writeFileSync(__dirname + '/../public/json/party_data.json', jsonParty, err => {
     if (err) {
       console.log('Error writing file', err)
     } else {
